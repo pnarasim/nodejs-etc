49b6dc9a9133cb84b7190819f496c3857a5f87a6
Priya Narasimhan <priya.narasimhan@gmail.com>
2015-10-30 12:19:59 +0530

    added a group by, so only unique advertisers show up made the drop down list, so we can choose 1 advertiser
    tbd:
    1. now, choose one advertiser, then query backend and get his data
    from all aggregators

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
  */
 
 var models = require("../models");
+var Sequelize = require('sequelize');
 
 exports.index = function(req, res) {
     console.log("AHA index");
@@ -11,8 +12,12 @@ exports.index = function(req, res) {
 
 exports.getadvertisers = function(req, res) {
     console.log("AHA get advs");
-    models.advertisers.findAll().then(function(result){
+        models.advertisers.findAll({
+        group: ['ad_addr']
+        }
+    ).then(function(result){
     console.log(result);
+    ///console.log(result.rows);
     res.render("index", {title: 'BLE advertisers', result: result});
     //res.json(result); 
     
